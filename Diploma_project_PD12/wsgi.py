e94bbd9868d1af8c64314f7e2e497bfa54806899
StasHunter92 <stashunter92@gmail.com>
2023-03-21 21:12:42 +0300

    Create base files

---
+++ Diploma_project_PD12/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for Diploma_project_PD12 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Load environment variables from .env
+load_dotenv()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Create Django WSGI application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Diploma_project_PD12.settings')
+application = get_wsgi_application()
