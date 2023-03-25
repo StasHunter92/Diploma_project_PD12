a292dbe408f67fd52d119cf22e39d45ec926fdcd
StasHunter92 <stashunter92@gmail.com>
2023-03-25 18:38:11 +0300

    Add dock-strings and comments

--- Diploma_project_PD12/asgi.py
+++ Diploma_project_PD12/asgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Set the DJANGO_SETTINGS_MODULE environment variable to the project's settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Diploma_project_PD12.settings')
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Create an ASGI application object using the project's settings module
 application = get_asgi_application()
