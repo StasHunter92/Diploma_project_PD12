c5de124ef1cc45cc9873b7df6939ea6527cbb185
StasHunter92 <stashunter92@gmail.com>
2023-03-26 13:40:41 +0300

    Fix password field display

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core.models import User
 
@@ -8,8 +9,24 @@ from core.models import User
 # Create models
 class CustomAdmin(UserAdmin):
     """Custom settings for admin panel"""
-    exclude: tuple[str] = ('password',)
-    readonly_fields: tuple[str] = ('date_joined', 'last_login')
+    fieldsets: tuple[tuple] = (
+        (None, {"fields": ("username",)}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    readonly_fields: tuple[str] = ('last_login', 'date_joined')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
