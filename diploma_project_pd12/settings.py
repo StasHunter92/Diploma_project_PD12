d07c0be27921bc6830826b41a02bf92d075c21bb
StasHunter92 <stashunter92@gmail.com>
2023-04-02 00:04:24 +0300

    Add static folder

--- diploma_project_pd12/settings.py
+++ diploma_project_pd12/settings.py
@@ -102,6 +102,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
